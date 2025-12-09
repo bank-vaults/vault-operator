6a5e6beac7da7d2b3ff2d47647a35c12bbf1069a
Bence Csati <bence.csati@axoflow.com>
2025-12-09 11:05:31 +0100

    feat: add utils pkg
    Signed-off-by: Bence Csati <bence.csati@axoflow.com>

--- pkg/apis/vault/v1alpha1/embedded_v1_test.go
+++ pkg/apis/vault/v1alpha1/embedded_v1_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2023 Bank-Vaults Maintainers
+// Copyright © 2025 Bank-Vaults Maintainers
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package v1alpha1
 import (
 	"testing"
 
+	"github.com/bank-vaults/vault-operator/pkg/utils"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -29,19 +30,19 @@ func TestToPodSpec(t *testing.T) {
 		Containers:                    []v1.Container{{Name: "main"}},
 		EphemeralContainers:           []v1.EphemeralContainer{{EphemeralContainerCommon: v1.EphemeralContainerCommon{Name: "eph"}}},
 		RestartPolicy:                 v1.RestartPolicyOnFailure,
-		TerminationGracePeriodSeconds: int64Ptr(42),
-		ActiveDeadlineSeconds:         int64Ptr(100),
+		TerminationGracePeriodSeconds: utils.To(int64(42)),
+		ActiveDeadlineSeconds:         utils.To(int64(100)),
 		DNSPolicy:                     v1.DNSClusterFirst,
 		NodeSelector:                  map[string]string{"k": "v"},
 		ServiceAccountName:            "svc",
 		DeprecatedServiceAccount:      "svc-deprecated",
-		AutomountServiceAccountToken:  boolPtr(true),
+		AutomountServiceAccountToken:  utils.To(true),
 		NodeName:                      "node",
 		HostNetwork:                   true,
 		HostPID:                       true,
 		HostIPC:                       true,
-		ShareProcessNamespace:         boolPtr(true),
-		SecurityContext:               &v1.PodSecurityContext{RunAsUser: int64Ptr(1000)},
+		ShareProcessNamespace:         utils.To(true),
+		SecurityContext:               &v1.PodSecurityContext{RunAsUser: utils.To(int64(1000))},
 		ImagePullSecrets:              []v1.LocalObjectReference{{Name: "pullsecret"}},
 		Hostname:                      "host",
 		Subdomain:                     "sub",
@@ -50,17 +51,17 @@ func TestToPodSpec(t *testing.T) {
 		Tolerations:                   []v1.Toleration{{Key: "key"}},
 		HostAliases:                   []v1.HostAlias{{IP: "127.0.0.1"}},
 		PriorityClassName:             "high",
-		Priority:                      int32Ptr(10),
+		Priority:                      utils.To(int32(10)),
 		DNSConfig:                     &v1.PodDNSConfig{},
 		ReadinessGates:                []v1.PodReadinessGate{{ConditionType: "ready"}},
-		RuntimeClassName:              strPtr("runtime"),
-		EnableServiceLinks:            boolPtr(false),
-		PreemptionPolicy:              preemptionPtr(v1.PreemptLowerPriority),
+		RuntimeClassName:              utils.To("runtime"),
+		EnableServiceLinks:            utils.To(false),
+		PreemptionPolicy:              utils.To(v1.PreemptLowerPriority),
 		Overhead:                      func() v1.ResourceList { qv, _ := resource.ParseQuantity("1"); return v1.ResourceList{"cpu": qv} }(),
 		TopologySpreadConstraints:     []v1.TopologySpreadConstraint{{MaxSkew: 1}},
-		SetHostnameAsFQDN:             boolPtr(true),
+		SetHostnameAsFQDN:             utils.To(true),
 		OS:                            &v1.PodOS{Name: "linux"},
-		HostUsers:                     boolPtr(false),
+		HostUsers:                     utils.To(false),
 		SchedulingGates:               []v1.PodSchedulingGate{{Name: "gate"}},
 		ResourceClaims:                []v1.PodResourceClaim{{Name: "claim"}},
 		Resources:                     &v1.ResourceRequirements{},
@@ -91,11 +92,3 @@ func TestToPodSpec(t *testing.T) {
 		require.Equal(t, eps.ServiceAccountName, ps.ServiceAccountName)
 	})
 }
-
-func int64Ptr(i int64) *int64                                  { return &i }
-func int32Ptr(i int32) *int32                                  { return &i }
-func boolPtr(b bool) *bool                                     { return &b }
-func strPtr(s string) *string                                  { return &s }
-func preemptionPtr(p v1.PreemptionPolicy) *v1.PreemptionPolicy { return &p }
-
-// Removed resourceQty function
